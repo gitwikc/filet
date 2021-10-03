77ae664ff37c3abc7f2ec2dd8777f1c5bae3c427
Sattwik <sahusattwik@gmail.com>
2021-10-03 22:28:41 +0530

    Ready for release: - Updated version info - Changed LICENSE file format (for inno setup) - Added larger icon (256x256) - Colorful error / warning messages and version info

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ from dirmaker import create_gitignore, make_dir_tree
 from preprocess import get_args
 from util import get_tree, print_tree
 
+from colorama import init as colorama_init, Fore
+
+
+colorama_init()
+
 
 if __name__ == "__main__":
     release_info = {
-        "version": "v1.6",
-        "date": "2021-07-28"
+        "version": "v1.7",
+        "date": "2021-10-03"
     }
     args = get_args()
 
     if args.version:
         print(
-            f"> Filet version\t\t{release_info['version']}\n> Release date\t\t{release_info['date']}\n{'-' * 40}")
+            f"> Filet version\t\t{Fore.YELLOW}{release_info['version']}{Fore.RESET}")
+        print(
+            f"> Release date\t\t{Fore.MAGENTA}{release_info['date']}{Fore.RESET}\n{'-' * 40}")
 
     try:
         tree = get_tree(args.struc)
@@ -21,10 +28,11 @@ if __name__ == "__main__":
             make_dir_tree(
                 parent_path=args.root, tree=tree))
     except IndexError as inx_ex:
-        print('Incorrect syntax. Please check filet structure')
+        print(f'{Fore.RED}Error: Incorrect syntax. Please check filet structure')
     except FileExistsError as fe_ex:
-        print('File/folder already exists in root directory')
+        print(f'{Fore.YELLOW}Warning: File/folder already exists in root directory')
     else:
-        print('\u2714 Successfully created file tree\n')
+        print(
+            f'{Fore.LIGHTGREEN_EX}\u2714 {Fore.LIGHTWHITE_EX}Successfully created file tree{Fore.RESET}\n')
         if not args.notree:
             print_tree(tree)
